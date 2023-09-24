c9ac208c1f0108879b6e94468553edb4882b6461
CamelKing <kongyukloong@gmail.com>
2023-09-24 20:15:08 +0800

    +git-prompt and git-completion

---
+++ bash/install-bash.sh
@@ -0,0 +1,5 @@
+# pull the git-prompt.sh from github repo
+curl -o ~/src/setup-pi-os-lite/bash/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+# pull the git-completion.bash from github repo
+curl -o ~/src/setup-pi-os-lite/bash/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
