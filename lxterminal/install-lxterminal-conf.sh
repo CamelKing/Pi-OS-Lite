ee38d7e12ace7208c9340bbc051915fe18a1197f
Camel King <kongyukloong@gmail.com>
2023-10-13 14:06:00 +0800

    ! lxterminal.conf install script

--- lxterminal/install-lxterminal-conf.sh
+++ lxterminal/install-lxterminal-conf.sh
@@ -16,53 +16,40 @@ function Install_lxterminal_config {
     local _copy_from="$(dirname $(readlink -f $0))" 
     local _program_name="LXTerminal"
     local _conf_file="lxterminal.conf"
-    local _file_location="$HOME/.config/lxterminal"
-    local _config="$_file_location/$_conf_file"
-    local _backup="$_file_location/$_conf_file.bak"
+    local _config="$HOME/.config/lxterminal/$_conf_file"
 
-    # Backup current config + remove the config file / symlink
+    # Backup existing config + remove the config file / symlink
+
     if [ -f "$_config" ]; then
 
-
-        local _from=( "$_config" )
-        local _to=( "$_backup" )
-        Copy_Files "$_test_mode" \
-            "Backing up $_conf_file" \
-            "${_from[@]}" "${_to[@]}"
-
         local _backup_cmd=( \
-            "cp -v -p $_config $_backup" \
+            "cp -v -p $_config $_config.bak" \
             "rm -v -f $_config" \
-            )
+        )
 
-        Execute_Commands_List "$_test_mode" \
-                "===> Backup $_program_name Configuration File" \
-                "${_backup_cmd[@]}"
+        Execute_Commands_List \
+            $_test_mode \
+            "===> Backup $_program_name configuration file" \
+            "${_backup_cmd[@]}"
 
-#         Execute "$Test_Mode" \
-                # "cp -v -p $_config $_backup" \
-                # "===> Backup $_program_name Configuration File"
-        #
-        # Execute "$Test_Mode" "rm -v -f $_config"
-#
     fi
 
     # Copy config file to the .sys directory and create symlink to it
 
-    Execute "$Test_Mode" \
-            "mkdir -v -p $_copy_to" \
-            "===> Copying $_program_name Configuration File" 
+    local _copy_cmd=( \
+        "mkdir -v -p $_copy_to" \
+        "cp -v -p $_copy_from/$_conf_file $_copy_to/$_conf_file" \
+    )
 
-    Execute "$Test_Mode" \
-            "cp -v -p $_copy_from/$_conf_file $_copy_to/$_conf_file" 
+    Execute_Commands_List \
+        $_test_mode \
+        "===> Copying $_program_name configuration file" \
+        "${_copy_cmd[@]}"
 
-    Execute "$Test_Mode" \
-            "mkdir -v -p $_file_location" 
-
-    Create_Symlinks "$_test_mode" \
-              "Creating symlinks for $_conf_file" \
-	            "$_copy_to/$_conf_file" \
-	            "$_config"
+    Create_Symlinks $_test_mode \
+                    $_conf_file \
+	                  "$_copy_to/$_conf_file" \
+	                  $_config
 
 }
 
@@ -76,7 +63,7 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 
     Print_Header_Banner $Test_Mode "$Project_Name" 
     
-    Install_lxterminal_config "$Test_Mode" "$HOME/.sys/lxterminal"
+    Install_lxterminal_config $Test_Mode "$HOME/.sys/lxterminal"
     
     Print_Footer_Banner "$Project_Name" 
 
