77d2cf6c45405766d3b7c390e84c7e9133db0f65
Camel King <kongyukloong@gmail.com>
2023-10-17 11:56:59 +0800

    ! finalised python install script

--- python/install-python.sh
+++ python/install-python.sh
@@ -8,61 +8,96 @@ source "$(dirname $0)/../share/common.sh"    # shared functions
 function Install_Python {
 
     # $1 test mode indicator, empty string means not test mode
-    # $2 Install to directory, default is the same dir as script file
+    # $2 python venv directory, default is $HOME/.sys/python
+    # $3 dev directory, where some python setting files will be stored
+    #                   default is $HOME/dev
 
     local _test_mode=$1
-    local _install_to="$HOME/.sys/python"
-    [ "$#" -gt 1 ] && _copy_to=$2    
+    local _venv_dir="$HOME/.sys/python"
+    [ "$#" -gt 1 ] && _venv_dir=$2    
+    local _dev_dir="$HOME/dev"
+    [ "$#" -gt 2 ] && _dev_dir=$3    
     local _install_from="$(dirname $(readlink -f $0))" 
     local _program_name="python"
+    local _pylintrc=".pylintrc"
 
-		# sudo apt install pylint - this was done automatically by Pi OS
-    sudo apt install pyhon3-full
-    sudo apt install flake8 
-    sudo apt install python3-autopep8 
-    sudo apt install python3-yapf # no bin file, overlapped autopep8
-    sudo apt install python3-pytest 
-    sudo apt install python3-coverage # no bin file
-		sudo apt install numpy # couldnt find in apt and already exist
-		sudo apt install python3-pandas 
-		sudo apt install python3-matplotlib # already exist
-    # jupyterlab
-		# sudo apt install pandas-datareader # not found, unless git 
-		# pip install beautifulsoup4
+    local _dirs_to_handle=( \
+        "$_venv_dir" \
+        "$_dev_dir" \
+    )
 
-#     # Backup existing config + remove the config file / symlink
-#
-    # if [ -f "$_config" ]; then
-#
-        # local _backup_cmd=( \
-            # "cp -v -p $_config $_config.bak" \
-            # "rm -v -f $_config" \
-        # )
-#
-        # Execute_Commands_List \
-            # $_test_mode \
-            # "===> Backup $_program_name configuration file" \
-            # "${_backup_cmd[@]}"
-#
-    # fi
-#
-    # # Copy config file to the .sys directory and create symlink to it
-#
-    # local _copy_cmd=( \
-        # "mkdir -v -p $_copy_to" \
-        # "cp -v -p $_copy_from/$_conf_file $_copy_to/$_conf_file" \
-    # )
-#
-    # Execute_Commands_List \
-        # $_test_mode \
-        # "===> Copying $_program_name configuration file" \
-        # "${_copy_cmd[@]}"
-#
-    # Create_Symlinks $_test_mode \
-                    # $_conf_file \
-										# "$_copy_to/$_conf_file" \
-										# $_config
-#
+    local _symlink_destination=( \
+        "$_venv_dir/$_pylintrc" \
+    )
+
+    local _symlink_target=( \
+        "$_dev_dir/$_pylintrc" \
+    )
+
+    local _copy_from=( \
+        "$_install_from/$_pylintrc" \
+    )
+
+    local _copy_to=( \
+        "$_venv_dir/$_pylintrc" \
+    )
+
+		# remove previous python venv
+    Remove_Symlinks $_test_mode \
+                    "previous $_program_name venv" \
+                    "${_symlink_target[@]}"
+
+    Remove_Directories $_test_mode \
+                       "previous $_program_name venv" \
+                       "${_dirs_to_handle[@]}"
+
+		# setup python venv
+
+    local _venv_setup_cmd=( \
+            "sudo apt install python3-full" \
+            "python -m venv  ~/.sys/python" \
+    )
+
+    Make_Directories $_test_mode \
+                     "new $_program_name venv" \
+                     "${_dirs_to_handle[@]}"
+
+    Execute_Commands_List \
+        $_test_mode \
+        "===> Setup new $_program_name venv" \
+        "${_venv_setup_cmd[@]}"
+
+    # installing python packages in VENV with pip
+    # must activate python venv then run pip
+
+    local _activate="source ~/.sys/python/bin/activate"
+    local _pip_install_cmd=( \
+            "$_activate && pip install -U pip" \
+            "$_activate && pip install flake8 autopep8 yapf" \
+            "$_activate && pip install pylint beautifulsoup4" \
+            "$_activate && pip install pytest coverage" \
+            "$_activate && pip install numpy pandas" \
+            "$_activate && pip install pandas-datareader matplotlib" \
+            "$_activate && pip install jupyterlab" \
+    )
+
+    Execute_Commands_List \
+        $_test_mode \
+        "===> Install packages for $_program_name" \
+        "${_pip_install_cmd[@]}"
+
+    # copy .pylintrc and create symlink for it   
+
+    Copy_Files $_test_mode \
+               "$_program_name pylint rc file" \
+	             "${_copy_from[@]}" \
+	             "${_copy_to[@]}"
+
+    Create_Symlinks $_test_mode \
+                    "$_program_name pylint rc file" \
+	                  "${_symlink_destination[@]}" \
+	                  "${_symlink_target[@]}"
+
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
@@ -75,7 +110,7 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 
     Print_Header_Banner $Test_Mode "$Project_Name" 
     
-    Install_Python $Test_Mode "$HOME/.sys/python"
+    Install_Python $Test_Mode "$HOME/.sys/python" "$HOME/dev"
     
     Print_Footer_Banner "$Project_Name" 
 
